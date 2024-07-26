336690071be8b65d1ba693ff4d210184a9d9d0e1
cheoljundev <cheoljundev@gmail.com>
2024-07-26 19:42:35 +0900

    Rest 인증 상태 영속하기 - SecurityContextRepository 설정하기

--- src/main/java/io/security/springsecuritymaster/controller/HomeController.java
+++ src/main/java/io/security/springsecuritymaster/controller/HomeController.java
@@ -1,9 +1,13 @@
 package io.security.springsecuritymaster.controller;
 
 
+import io.security.springsecuritymaster.domain.dto.AccountDto;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+@Slf4j
 @Controller
 public class HomeController {
 	@GetMapping(value="/")
